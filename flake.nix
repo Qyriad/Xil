1762d698c831ab9f19db0f9b61916e7eb97b8d0e
Qyriad <qyriad@qyriad.me>
2024-02-08 07:28:05 -0700

    append `nix-unstable` to xil's pname when applicable

--- flake.nix
+++ flake.nix
@@ -7,14 +7,20 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs { inherit system; };
+      inherit (pkgs) lib;
 
       xil = pkgs.callPackage ./package.nix {
         cppitertools = pkgs.callPackage ./cppitertools.nix { };
       };
 
-      xilForNixUnstable = xil.override {
-        nix = pkgs.nixVersions.unstable;
-      };
+      xilForNixUnstable = lib.pipe xil [
+        (xil: xil.override {
+          nix = pkgs.nixVersions.unstable;
+        })
+        (xil: xil.overrideAttrs (prev: {
+          pname = "${prev.pname}-nix-unstable";
+        }))
+      ];
 
     in {
       packages = {
