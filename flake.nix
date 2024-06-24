563229c826e6b37a64e72d29154ab96e8ee18fcb
Qyriad <qyriad@qyriad.me>
2024-06-24 07:44:24 -0600

    reformat flake.nix a little

--- flake.nix
+++ flake.nix
@@ -4,21 +4,23 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }: flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs { inherit system; };
 
-      xil = pkgs.callPackage ./package.nix {
-        cppitertools = pkgs.callPackage ./cppitertools.nix { };
-      };
+    xil = pkgs.callPackage ./package.nix {
+      cppitertools = pkgs.callPackage ./cppitertools.nix { };
+    };
 
-    in {
-      packages = {
-        default = xil;
-        inherit xil;
-      };
-      devShells.default = pkgs.callPackage xil.mkShell { };
-      checks = self.outputs.packages.${system};
-    }) # eachDefaultSystem
-  ; # outputs
+  in {
+    packages = {
+      default = xil;
+      inherit xil;
+    };
+    devShells.default = pkgs.callPackage xil.mkShell { };
+    checks = self.outputs.packages.${system};
+  }); # outputs
 }
